5fffdc2a3d2f2fc59ec4d6ca3b4b20221c64070a
Daniel Grychtol <grychtol.daniel@gmail.com>
2019-08-26 13:56:33 +0200

    Remove unnecessary comments

--- src/animations/animations.js
+++ src/animations/animations.js
@@ -15,5 +15,3 @@ timeline.from(appContainer, 0.3, {
   scale: 0.8,
   ease: Power4.easeOut
 });
-//   .to(appContainer, 1, { top: -100, ease: Power4.easeOut });
-//   .to(appContainer, 0.9, { top: 0, ease: Back.easeOut.config(1.7) });
